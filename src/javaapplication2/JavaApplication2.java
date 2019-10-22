34cbcc293cfea5ab788dee05d4c1b520e4f5920d
hp <hp@VENOM>
2019-10-22 18:11:42 +0530

    task1

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author hp
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String venom = "String(Variable)";
+        String venom1 = "int(Variable)";
+        String venom2 = "String(Static)";
+        String venom3 = "int(Variable)";
+        System.out.println("Name : " + venom);
+        System.out.println("Age : " + venom1);
+        System.out.println("School : " + venom2);
+        System.out.println("Grade : " + venom3);
+        
+    }
+    
+}
